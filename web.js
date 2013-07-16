447a27eaae749862fe5bd62e8376e238289a9a93
Reinhard Haug <haug.reinhard@gmail.com>
2013-07-16 14:20:47 +0000

    read2

--- web.js
+++ web.js
@@ -1,12 +1,19 @@
 var express = require('express');
-
+var fs j= require('fs');
 var app = express.createServer(express.logger());
 
+
+fs.readFileSync('./index.html', function (err,html){
+	if (err)
+{
+	throw err;
+}
 
 
 app.get('/', function(request, response) {
  
  	response.send('hallo');
+})
 });
 
 var port = process.env.PORT || 5000;
